9e4aee515b1452ddfbc49efbf7b48f0f2d54725a
Sayem Saruwar <sayem.saruwar@gmail.com>
2026-01-15 23:21:18 +0600

    Added Lab2

---
+++ Lab2/HomeTask1.java
@@ -0,0 +1,21 @@
+//Task1
+import java.util.Scanner;
+public class HomeTask1{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter an integer number: ");
+    int number = sc.nextInt();
+    if(number%5==0 && number%7==0){
+      System.out.println("Invalid: Divisible by both");
+    }
+    else if(number%5==0){
+      System.out.println("Divisible by 5 only");
+    }
+    else if(number%7==0){
+      System.out.println("Divisible by 7 only");
+    }
+    else{
+      System.out.println("No");
+    }
+  }
+}
